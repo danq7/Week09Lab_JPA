c40bbbbaee0f9e2925be3c6885d88c1d3a2301f5
839225 <839225@SAIT225243IT.ACDM.DS.SAIT.CA>
2020-11-08 17:50:28 -0700

    Week 9 Lab Activation Converted the starting point from using JDBC to JPA

--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -1,45 +1,54 @@
 package services;
 
 import database.UserDB;
-import models.User;
+import models.Users;
 import java.util.List;
 
 public class UserService {
 
-    private UserDB userDB;
+    private UserDB userDB = new UserDB();
 
-    public UserService() {
-        userDB = new UserDB();
-    }
+    public Users get(String username) throws Exception {
 
-    public User get(String username) throws Exception {
         return userDB.getUser(username);
+
     }
 
-    public List<User> getAll() throws Exception {
-        return userDB.getAll();
+    public List<Users> getAll() throws Exception {
+
+        List<Users> usersList = userDB.getAll();
+        return usersList;
+
     }
 
-    public int update(String username, String password, String firstname, String lastname, String email) throws Exception {
-        User user = get(username);
+    public void update(String username, String password, String firstname, String lastname, String email) throws Exception {
+
+        Users user = userDB.getUser(username);
         user.setPassword(password);
         user.setFirstname(firstname);
         user.setLastname(lastname);
         user.setEmail(email);
-        return userDB.update(user);
+        userDB.update(user);
+
     }
 
-    public int delete(String username) throws Exception {
-        User deletedUser = userDB.getUser(username);
+    public void delete(String username) throws Exception {
+
+        Users deletedUser = userDB.getUser(username);
         // do not allow the admin to be deleted
-        if (deletedUser.getUsername().equals("admin")) {
-            return 0;
+        if (!deletedUser.getUsername().equals("admin")) {
+            userDB.delete(deletedUser);
         }
-        return userDB.delete(deletedUser);
+
     }
 
-    public int insert(String username, String password, String firstname, String lastname, String email) throws Exception {
-        User user = new User(username, password, firstname, lastname, email);
-        return userDB.insert(user);
+    public void insert(String username, String password, String firstname, String lastname, String email) throws Exception {
+        Users user = new Users(username, password);
+        user.setFirstname(firstname);
+        user.setLastname(lastname);
+        user.setEmail(email);
+        userDB.insert(user);
+
     }
+
 }
